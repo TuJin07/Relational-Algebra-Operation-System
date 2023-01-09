41cd638255c2fe266572c7b2ee828cf45ba3c97c
TuJin <1349222354@qq.com>
2023-01-09 20:03:29 +0800

    词法分析完善

--- operation_system/src/main/java/com/example/operation_system/service/impl/LexicalAnalysisServiceImpl.java
+++ operation_system/src/main/java/com/example/operation_system/service/impl/LexicalAnalysisServiceImpl.java
@@ -4,6 +4,9 @@ import com.example.operation_system.service.LexicalAnalysisService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * @program: operation_system
@@ -19,6 +22,8 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
 
     private boolean isIllegal;
 
+    private List<String> result;
+
     @Override
     public boolean analysis(String expression) {
         log.info("开始对表达式进行词法分析...");
@@ -27,6 +32,7 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
             return false;
         }
         isIllegal = true;
+        result = new ArrayList<>();
         code = (expression + "#").toCharArray();
         startState(0, 0);
         log.info("词法分析完成，结果为：" + isIllegal);
@@ -92,111 +98,154 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
             isIllegal = false;
             String cur = new String(code, basicPointer, currentPointer - basicPointer);
             log.info("<错误, " + cur + ">");
+            result.add(cur);
             startState(basicPointer + 1, currentPointer + 1);
         }
     }
 
     private void orOperatorState(int i) {
         log.info("<或运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void andOperatorState(int i) {
         log.info("<与运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void diffOperatorState(int i) {
         log.info("<差运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void prodOperatorState(int i) {
         log.info("<笛卡尔积运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void divOperatorState(int i) {
         log.info("<除运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void joinOperatorState(int i) {
         log.info("<连接运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void projectOperatorState(int i) {
         log.info("<投影运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void selectOperatorState(int i) {
         log.info("<选择运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void comma(int i) {
         log.info("<逗号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalAndState(int i) {
         log.info("<条件与运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalOrState(int i) {
         log.info("<条件或运算符, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalEqualState(int i) {
         log.info("<等号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalInequalityState(int i) {
         log.info("<不等号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalGreaterThanSign(int i) {
         log.info("<大于号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalLessThanSign(int i) {
         log.info("<小于号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void conditionalGreaterOrEqualSign(int i) {
         log.info("<大于等于号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);    }
 
     private void conditionalLessOrEqualSign(int i) {
         log.info("<小于等于号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void lParentState(int i) {
         log.info("<左括号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void rParentState(int i) {
         log.info("<右括号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void lBracket(int i) {
         log.info("<左方括号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
     private void rBracket(int i) {
         log.info("<右方括号, " + code[i] + ">");
+        String tmp = String.valueOf(code[i]);
+        result.add(tmp);
         startState(i + 1, i + 1);
     }
 
@@ -206,6 +255,7 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
         } else {
             String curIdent = new String(code, basicPointer, currentPointer - basicPointer);
             log.info("<标识符, " + curIdent + ">");
+            result.add("id");
             startState(currentPointer, currentPointer);
         }
     }
@@ -218,6 +268,7 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
         } else {
             String curInt = new String(code, basicPointer, currentPointer - basicPointer);
             log.info("<实数, " + curInt + ">");
+            result.add("num");
             startState(currentPointer, currentPointer);
         }
     }
@@ -227,6 +278,7 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
             isIllegal = false;
             String curInt = new String(code, basicPointer, currentPointer - basicPointer);
             log.info("<错误, " + curInt + ">");
+            result.add(curInt);
             startState(currentPointer, currentPointer);
         } else {
             floatState(basicPointer, currentPointer + 1);
@@ -239,6 +291,7 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
         } else {
             String curFloat = new String(code, basicPointer, currentPointer - basicPointer);
             log.info("<浮点数, " + curFloat + ">");
+            result.add("num");
             startState(currentPointer, currentPointer);
         }
     }
@@ -251,10 +304,15 @@ public class LexicalAnalysisServiceImpl implements LexicalAnalysisService {
         return Character.isDigit(code[index]);
     }
 
+    public List<String> getResult() {
+        return result;
+    }
+
     public static void main(String[] args) {
         LexicalAnalysisService service = new LexicalAnalysisServiceImpl();
-//        String expression = "A∩Student∪B−(C×Course)⋈σ[A=10.50][Go]×π[Sno,Cno12][B]×σ[A=10AB∧B=20∨C=39][A∩B]";
-        String expression = "A";
+        String expression = "A∩Student∪B−(C×Course)⋈σ[A=10.50][Go]×π[Sno,Cno12][B]×σ[A=10AB∧B=20∨C=39][A∩B]";
+//        String expression = "A";
         service.analysis(expression);
+        System.out.println(service.getResult());
     }
 }
